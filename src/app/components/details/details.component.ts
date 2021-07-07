2aa4a04cb57e874ab149c9bcc4798d9831071496
Luiz <luizotcunha@gmail.com>
2021-07-07 12:50:05 -0400

    component create

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -13,7 +13,6 @@ import { TodoService } from 'src/app/services/todo.service';
 })
 export class DetailsComponent implements OnInit {
   taskId: number;
-  toDo: Todo;
   formToDo: FormGroup;
   toDoTemp;
 
@@ -22,39 +21,34 @@ export class DetailsComponent implements OnInit {
     public actRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.createForm();
     const id = this.actRoute.snapshot.params['id'];
     if(id) {
       this.service.getToDo(id).subscribe(data => {
-      console.log(data);
+        this.loadForm(data);
       })
+
     }
-    // console.log(this.toDoTemp);
-    // this.createForm();
+    console.log(this.formToDo)
   }
 
-  // loadForm() {
-  //   this.formToDo = this.fb.group({
-  //     responsible: [this.toDo.responsible, [Validators.required]],
-  //     phone: [this.toDo.phone, [Validators.required]],
-  //     email: [this.toDo.email, [Validators.required]],
-  //     description: [this.toDo.description, [Validators.required]],
-  //     dateEnd: [this.toDo.dateEnd, [Validators.required]],
-  //     hourEnd: [this.toDo.hourEnd, [Validators.required]],
-  //   })
-  // }
+  loadForm(data) {
+    this.formToDo.get('responsible')?.setValue(data.responsible);
+    this.formToDo.get('phone')?.setValue(data.phone);
+    this.formToDo.get('email')?.setValue(data.email);
+    this.formToDo.get('description')?.setValue(data.description);
+    this.formToDo.get('dateEnd')?.setValue(data.dateEnd);
+    this.formToDo.get('hourEnd')?.setValue(data.hourEnd);
+  }
 
   createForm() {
     this.formToDo = this.fb.group({
-      responsible: [null, [Validators.required]],
+      hourEnd: [null, [Validators.required]],
       phone: [null, [Validators.required]],
       email: [null, [Validators.required]],
       description: [null, [Validators.required]],
       dateEnd: [null, [Validators.required]],
-      hourEnd: [null, [Validators.required]],
     })
   }
 
 }
-
-
-
